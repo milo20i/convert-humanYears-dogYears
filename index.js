7aa835494364087ea949304ff0778ab15a04ee29
Danilo De Matteis <62311664+milo20i@users.noreply.github.com>
2021-11-15 11:18:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const myAge = 31;
+// I create a variable that is equal of my age
+
+let earlyYears = 2;
+/* I create a variable of the first 2 years of a dog age */
+
+earlyYears = earlyYears* 10.5;
+
+let laterYears = myAge - 2;
+/* I wrote a new variable that I subtract my age to a fist 2 years dog */ 
+
+laterYears *= 4;
+const myAgeInDogYears = earlyYears + laterYears;
+/* I add the variable that stored my age to the variable that store years dog */
+
+const myName = 'Danilo De Matteis'.toLowerCase();
+
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
